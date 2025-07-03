3297220c78f7bb6b5a0028f51c09088924ee0c8f
Виктория <v.dorzhieva@bnovo.ru>
2025-07-03 20:05:39 +0300

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,8 @@ def browser():
     options = Options()
     options.add_argument("--start-maximized")
     options.add_argument("--disable-notifications")
-    options.page_load_strategy = "eager"
+    # options.page_load_strategy = "eager"
+    # options.add_argument('--headless=new')
 
     driver = webdriver.Chrome(options=options)
     yield driver
